b19ede76845371063ea0558cd0b26dafd1a8eca5
Andreas Jim-Hartmann <andreas.hartmann@becompany.ch>
2016-08-19 10:05:14 +0200

    * Add more default parsers. * Prepare for publishing to sonatype repo.

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -7,3 +7,7 @@ addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0")
 addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "1.0.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-ghpages" % "0.5.4")
+
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
+
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "1.1")
--- src/main/scala/ch/becompany/akka/io/csv/Parser.scala
+++ src/main/scala/ch/becompany/akka/io/csv/Parser.scala
@@ -1,13 +1,33 @@
 package ch.becompany.akka.io.csv
 
+import scala.util.{Success, Try}
+
 trait Parser[T] {
   def apply(s: String): Either[String, T]
+}
+
+trait TryParser[T] extends Parser[T] {
+
+  def parse(s: String): T
+
+  def apply(s: String): Either[String, T] =
+    Try(parse(s)).transform(
+      s => Success(Right(s)),
+      f => Success(Left(f.getMessage))).get
 }
 
 object Parsers {
 
   implicit val stringParser: Parser[String] = new Parser[String] {
     def apply(s: String): Either[String, String] = Right(s)
+  }
+
+  implicit val intParser: Parser[Int] = new TryParser[Int] {
+    def parse(s: String): Int = s.toInt
+  }
+
+  implicit val longParser: Parser[Long] = new TryParser[Long] {
+    def parse(s: String): Long = s.toLong
   }
 
 }
